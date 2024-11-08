86a8999ec0d480ecfdfaf57c0129e47dcc678306
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-11-08 16:59:12 -0300

    db: add new migrations

---
+++ drizzle/0002_jittery_the_executioner.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "match_results" RENAME COLUMN "winnerEnergies" TO "winner_energies";--> statement-breakpoint
+ALTER TABLE "match_results" RENAME COLUMN "loserEnergies" TO "loser_energies";
