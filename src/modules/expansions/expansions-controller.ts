29beee8755a53d195905b91b2522a0f56bb6fb08
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-11-08 17:04:33 -0300

    feat: add missing handlers to ExpansionsController

--- src/modules/expansions/expansions-controller.ts
+++ src/modules/expansions/expansions-controller.ts
@@ -1,10 +1,40 @@
 import { Request, Response } from "express";
+import { expansions } from "../../db/schema";
 import { CreateExpansionService } from "./services/create-expansion-service";
+import { DeleteExpansionService } from "./services/delete-expansion-service";
+import { GetExpansionService } from "./services/get-expansion-service";
+import { GetManyExpansionsService } from "./services/get-many-expansions-service";
+import { UpdateExpansionService } from "./services/update-expansion-service";
+
+type Expansion = typeof expansions.$inferSelect;
 
 export class ExpansionsController {
   create = async (req: Request, res: Response) => {
-    const data = req.body;
-    await new CreateExpansionService().execute(data);
-    res.json({ success: true });
+    await new CreateExpansionService().execute(req.body);
+    res.json({ success: true, message: "Expansion created." });
+  };
+
+  get = async (req: Request, res: Response) => {
+    const { id, limit, offset } = req.query;
+    let data: Expansion | Expansion[] | null = null;
+
+    data = id
+      ? await new GetExpansionService().execute(id)
+      : await new GetManyExpansionsService().execute({
+          limit,
+          offset
+        });
+
+    res.json({ success: true, data });
+  };
+
+  update = async (req: Request, res: Response) => {
+    await new UpdateExpansionService().execute(req.body);
+    res.json({ success: true, message: "Expansion updated." });
+  };
+
+  delete = async (req: Request, res: Response) => {
+    await new DeleteExpansionService().execute(req.query.id);
+    res.json({ success: true, message: "Expansion deleted." });
   };
 }
