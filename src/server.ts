4261873c087d6b5f886c159c7d1f40e89d4433ad
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-11-08 17:06:43 -0300

    feat(server): use cardsRouter

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import express from "express";
 import helmet from "helmet";
 import { env } from "./lib/env";
+import { cardsRouter } from "./modules/cards/cards-router";
 import { expansionsRouter } from "./modules/expansions/expansions-router";
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(helmet());
 app.use(cors({ origin: env.ORIGIN }));
 app.use(express.json());
 app.use(expansionsRouter);
+app.use(cardsRouter);
 
 app.listen(3000, () => {
   console.log(`Server running on http://localhost:${env.PORT}`);
