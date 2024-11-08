29393e439de3e89fd463a29835c7daf6144fb9da
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-11-08 17:04:54 -0300

    feat: add missing routes to expansionsRouter

--- src/modules/expansions/expansions-router.ts
+++ src/modules/expansions/expansions-router.ts
@@ -5,5 +5,8 @@ const expansionsController = new ExpansionsController();
 const expansionsRouter = Router();
 
 expansionsRouter.post("/expansions", expansionsController.create);
+expansionsRouter.get("/expansions", expansionsController.get);
+expansionsRouter.patch("/expansions", expansionsController.update);
+expansionsRouter.delete("/expansions", expansionsController.delete);
 
 export { expansionsRouter };
