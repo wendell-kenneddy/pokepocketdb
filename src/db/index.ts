155d48da6804cdb37b2ded801590ca70b29d0d29
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-11-21 17:05:01 -0300

    db: setup drizzle queries api

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { env } from "../lib/env";
+import * as schema from "./schema";
 
 export const client = postgres(env.DATABASE_URL);
-export const db = drizzle(client, { logger: true });
+export const db = drizzle(client, { logger: true, schema });
