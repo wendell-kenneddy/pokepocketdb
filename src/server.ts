3b1f4f271a0bb54852b3570681e02786c1671866
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-11-21 17:05:24 -0300

    feat(server): add missing routes and middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,24 @@
 import cors from "cors";
 import express from "express";
+import "express-async-errors";
 import helmet from "helmet";
 import { env } from "./lib/env";
+import { errorHandlerMiddleware } from "./middlewares/error-handler-middleware";
 import { cardsRouter } from "./modules/cards/cards-router";
 import { expansionsRouter } from "./modules/expansions/expansions-router";
+import { matchResultsRouter } from "./modules/matches/match-results-router";
 
 const app = express();
 
 app.use(helmet());
 app.use(cors({ origin: env.ORIGIN }));
 app.use(express.json());
+
 app.use(expansionsRouter);
 app.use(cardsRouter);
+app.use(matchResultsRouter);
+app.use(errorHandlerMiddleware);
 
-app.listen(3000, () => {
+app.listen(env.PORT, () => {
   console.log(`Server running on http://localhost:${env.PORT}`);
 });
