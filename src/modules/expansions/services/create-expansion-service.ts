31cefe2881b5c66d34491c08a79ac92e9c8e2886
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-11-22 16:51:27 -0300

    feat(create-expansion-service): add more strict validation and return expansion id

--- src/modules/expansions/services/create-expansion-service.ts
+++ src/modules/expansions/services/create-expansion-service.ts
@@ -2,11 +2,19 @@ import { createInsertSchema } from "drizzle-zod";
 import { db } from "../../../db";
 import { expansions } from "../../../db/schema";
 
-const createExpansionSchema = createInsertSchema(expansions);
+const createExpansionSchema = createInsertSchema(expansions, {
+  name: (s) => s.name.trim().min(1)
+}).required({
+  name: true
+});
 
 export class CreateExpansionService {
   async execute(data: unknown) {
     const { name } = createExpansionSchema.parse(data);
-    await db.insert(expansions).values({ name });
+    const id = await db
+      .insert(expansions)
+      .values({ name })
+      .returning({ id: expansions.id });
+    return id;
   }
 }
