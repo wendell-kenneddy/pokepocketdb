7c359e358209aa306f381f117d250688cfe15eb0
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-11-22 16:46:00 -0300

    db: update schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,12 +1,12 @@
 import { createId } from "@paralleldrive/cuid2";
-import { relations } from "drizzle-orm";
 import {
   boolean,
   integer,
   pgEnum,
   pgTable,
   text,
-  timestamp
+  timestamp,
+  unique
 } from "drizzle-orm/pg-core";
 
 export const pokemonTypesEnum = pgEnum("card_types", [
@@ -30,31 +30,26 @@ export const cardCategoriesEnum = pgEnum("card_categories", [
 
 export const expansions = pgTable("expansions", {
   id: text("id").primaryKey().$defaultFn(createId),
-  name: text("name").notNull(),
+  name: text("name").notNull().unique(),
   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow()
 });
 
-export const expansionsRelations = relations(expansions, ({ many }) => ({
-  cards: many(cards)
-}));
-
-export const cards = pgTable("cards", {
-  id: text("id").primaryKey().$defaultFn(createId),
-  name: text("name").notNull(),
-  category: cardCategoriesEnum("category").notNull(),
-  type: pokemonTypesEnum("type"),
-  expansionId: text("expansion_id")
-    .notNull()
-    .references(() => expansions.id),
-  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow()
-});
-
-export const cardsRelations = relations(cards, ({ one }) => ({
-  expansion: one(expansions, {
-    fields: [cards.expansionId],
-    references: [expansions.id]
+export const cards = pgTable(
+  "cards",
+  {
+    id: text("id").primaryKey().$defaultFn(createId),
+    name: text("name").notNull(),
+    category: cardCategoriesEnum("category").notNull(),
+    type: pokemonTypesEnum("type"),
+    expansionId: text("expansion_id")
+      .notNull()
+      .references(() => expansions.id, { onDelete: "cascade" }),
+    createdAt: timestamp("created_at", { withTimezone: true }).defaultNow()
+  },
+  (t) => ({
+    unq: unique().on(t.name, t.expansionId)
   })
-}));
+);
 
 export const matchResults = pgTable("match_results", {
   id: text("id").primaryKey().$defaultFn(createId),
@@ -68,56 +63,21 @@ export const matchResults = pgTable("match_results", {
   winnerEnergies: pokemonTypesEnum("winner_energies").array().notNull(),
   winnerLevel: integer("winner_level").notNull(),
   loserLevel: integer("loser_level").notNull(),
-  loserName: text("winner_name").notNull(),
+  loserName: text("loser_name").notNull(),
   loserConcede: boolean("loser_concede").notNull(),
   loserPoints: integer("loser_points").notNull(),
   loserEnergies: pokemonTypesEnum("loser_energies").array().notNull(),
   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow()
 });
 
-export const matchResultsRelations = relations(matchResults, ({ many }) => ({
-  matchWinnerCards: many(matchWinnerCards),
-  matchLoserCards: many(matchLoserCards)
-}));
-
-export const matchWinnerCards = pgTable("match_winner_cards", {
+export const matchCards = pgTable("match_cards", {
   id: text("id").primaryKey().$defaultFn(createId),
   matchId: text("match_id")
     .notNull()
-    .references(() => matchResults.id),
+    .references(() => matchResults.id, { onDelete: "cascade" }),
   cardId: text("card_id")
     .notNull()
-    .references(() => cards.id),
+    .references(() => cards.id, { onDelete: "cascade" }),
+  winnerCard: boolean("winner_card").notNull(),
   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow()
 });
-
-export const matchWinnerCardsRelations = relations(
-  matchWinnerCards,
-  ({ one }) => ({
-    card: one(cards, {
-      fields: [matchWinnerCards.cardId],
-      references: [cards.id]
-    })
-  })
-);
-
-export const matchLoserCards = pgTable("match_loser_cards", {
-  id: text("id").primaryKey().$defaultFn(createId),
-  matchId: text("match_id")
-    .notNull()
-    .references(() => matchResults.id),
-  cardId: text("card_id")
-    .notNull()
-    .references(() => cards.id),
-  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow()
-});
-
-export const matchLoserCardsRelations = relations(
-  matchLoserCards,
-  ({ one }) => ({
-    card: one(cards, {
-      fields: [matchLoserCards.cardId],
-      references: [cards.id]
-    })
-  })
-);
