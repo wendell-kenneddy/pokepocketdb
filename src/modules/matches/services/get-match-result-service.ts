283558b8549f1b9bc6959e0a56fdcbf44cd55fa0
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-11-22 17:00:13 -0300

    feat(get-match-result-service): update service logic to actually work

--- src/modules/matches/services/get-match-result-service.ts
+++ src/modules/matches/services/get-match-result-service.ts
@@ -1,6 +1,6 @@
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 import { db } from "../../../db";
-import { matchResults } from "../../../db/schema";
+import { cards, matchCards, matchResults } from "../../../db/schema";
 import { uuidSchema } from "../../../lib/uuid-schema";
 
 type MatchResult = typeof matchResults.$inferSelect;
@@ -11,33 +11,64 @@ export interface FullMatchResult extends MatchResult {
 }
 
 export class GetMatchResultService {
-  async execute(matchId: unknown): Promise<FullMatchResult | undefined> {
+  async execute(matchId: unknown) {
     const parsedMatchId = uuidSchema.parse(matchId);
-    const matchResult = await db.query.matchResults.findFirst({
-      where: eq(matchResults.id, parsedMatchId),
-      with: {
-        matchWinnerCards: {
-          with: {
-            card: {
-              columns: {
-                name: true,
-                id: true
-              }
-            }
-          }
-        },
-        matchLoserCards: {
-          with: {
-            card: {
-              columns: {
-                name: true,
-                id: true
-              }
-            }
-          }
-        }
-      }
-    });
-    return matchResult;
+    const utilizedCards = db.$with("utilized_cards").as(
+      db
+        .select({
+          cardId: matchCards.cardId,
+          cardName: cards.name,
+          winnerCard: matchCards.winnerCard
+        })
+        .from(matchCards)
+        .innerJoin(cards, eq(matchCards.cardId, cards.id))
+        .where(eq(matchCards.matchId, parsedMatchId))
+    );
+    const winnerDeck = db.$with("winner_cards").as(
+      db
+        .select({
+          deck: sql/*sql*/ `
+            JSON_AGG(
+              JSON_BUILD_OBJECT(
+                'cardId', ${utilizedCards.cardId},
+                'cardName', ${utilizedCards.cardName},
+                'winnerCard', ${utilizedCards.winnerCard}
+              )
+            )
+          `.as("winner_deck")
+        })
+        .from(utilizedCards)
+        .where(eq(utilizedCards.winnerCard, true))
+    );
+    const loserDeck = db.$with("loser_cards").as(
+      db
+        .select({
+          deck: sql/*sql*/ `
+            JSON_AGG(
+              JSON_BUILD_OBJECT(
+                'cardId', ${utilizedCards.cardId},
+                'cardName', ${utilizedCards.cardName},
+                'winnerCard', ${utilizedCards.winnerCard}
+              )
+            )
+          `.as("loser_deck")
+        })
+        .from(utilizedCards)
+        .where(eq(utilizedCards.winnerCard, false))
+    );
+    const result = await db
+      .with(utilizedCards, winnerDeck, loserDeck)
+      .select({
+        ...getTableColumns(matchResults),
+        winnerDeck: sql/*sql*/ `
+          (SELECT ${winnerDeck.deck} FROM ${winnerDeck})
+        `.as("winner_deck"),
+        loserDeck: sql/*sql*/ `
+          (SELECT ${loserDeck.deck} FROM ${loserDeck})
+        `.as("loser_deck")
+      })
+      .from(matchResults)
+      .where(eq(matchResults.id, parsedMatchId));
+    return result[0];
   }
 }
