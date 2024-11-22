1f5641d725549b67e6fab3c6d9bf4546b62acff8
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-11-22 16:53:51 -0300

    refactor(get-many-expansions-service): change asc to ascOrder

--- src/modules/expansions/services/get-many-expansions-service.ts
+++ src/modules/expansions/services/get-many-expansions-service.ts
@@ -6,18 +6,18 @@ import { expansions } from "../../../db/schema";
 const querySchema = z.object({
   page: z.coerce.number(),
   limit: z.coerce.number(),
-  asc: z.coerce.boolean()
+  ascOrder: z.coerce.boolean()
 });
 
 export class GetManyExpansionsService {
   async execute(data: unknown) {
-    const { page, limit, asc } = querySchema.parse(data);
+    const { page, limit, ascOrder } = querySchema.parse(data);
     const rows = await db
       .select()
       .from(expansions)
       .limit(limit)
       .offset((page - 1) * limit)
-      .orderBy(asc ? expansions.createdAt : desc(expansions.createdAt));
+      .orderBy(ascOrder ? expansions.createdAt : desc(expansions.createdAt));
     return rows;
   }
 }
