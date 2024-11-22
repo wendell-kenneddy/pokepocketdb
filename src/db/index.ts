d27ca3359e48146c0f00505bb65096185fdfdfaa
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-11-22 18:49:24 -0300

    refactor(db): log queries only on dev mode

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,8 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { env } from "../lib/env";
-import * as schema from "./schema";
 
 export const client = postgres(env.DATABASE_URL);
-export const db = drizzle(client, { logger: true, schema });
+export const db = drizzle(client, {
+  logger: process.env.NODE_ENV == "development"
+});
