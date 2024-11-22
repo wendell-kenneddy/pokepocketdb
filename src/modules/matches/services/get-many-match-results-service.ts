cf30809dd735a55bb572782eeeead1d9573b4ecf
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-11-22 18:50:08 -0300

    feat(get-many-match-results-service): return more columns and add order filter

--- src/modules/matches/services/get-many-match-results-service.ts
+++ src/modules/matches/services/get-many-match-results-service.ts
@@ -1,10 +1,12 @@
+import { desc } from "drizzle-orm";
 import z from "zod";
 import { db } from "../../../db";
 import { matchResults } from "../../../db/schema";
 
 const getManyMatchResultsSchema = z.object({
   limit: z.coerce.number(),
-  page: z.coerce.number()
+  page: z.coerce.number(),
+  ascOrder: z.coerce.boolean()
 });
 
 type MatchResult = typeof matchResults.$inferSelect;
@@ -20,19 +22,24 @@ export type MatchResultsPage = Pick<
 
 export class GetManyMatchResultsServie {
   async execute(data: unknown): Promise<MatchResultsPage[]> {
-    const { limit, page } = getManyMatchResultsSchema.parse(data);
+    const { limit, page, ascOrder } = getManyMatchResultsSchema.parse(data);
     const rows = await db
       .select({
         id: matchResults.id,
+        turns: matchResults.turns,
         winnerName: matchResults.winnerName,
         winnerEnergies: matchResults.winnerEnergies,
         loserName: matchResults.loserName,
         loserEnergies: matchResults.loserEnergies,
-        loserConcede: matchResults.loserConcede
+        loserConcede: matchResults.loserConcede,
+        createdAt: matchResults.createdAt
       })
       .from(matchResults)
       .limit(limit)
-      .offset(limit * (page - 1));
+      .offset(limit * (page - 1))
+      .orderBy(
+        ascOrder ? matchResults.createdAt : desc(matchResults.createdAt)
+      );
     return rows;
   }
 }
