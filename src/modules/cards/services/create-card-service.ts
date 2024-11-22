3e2ef5633f4025ed1101861d2dabdb3aea02b3f4
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-11-22 16:56:01 -0300

    feat(create-card-service): add more validations and return created card id

--- src/modules/cards/services/create-card-service.ts
+++ src/modules/cards/services/create-card-service.ts
@@ -2,11 +2,19 @@ import { createInsertSchema } from "drizzle-zod";
 import { db } from "../../../db";
 import { cards } from "../../../db/schema";
 
-const insertCardSchema = createInsertSchema(cards);
+const insertCardSchema = createInsertSchema(cards, {
+  name: (s) => s.name.trim().min(3)
+});
 
 export class CreateCardService {
   async execute(data: unknown) {
     const { name, category, expansionId, type } = insertCardSchema.parse(data);
-    await db.insert(cards).values({ name, category, expansionId, type });
+    const id = await db
+      .insert(cards)
+      .values({ name, category, expansionId, type })
+      .returning({
+        id: cards.id
+      });
+    return id;
   }
 }
