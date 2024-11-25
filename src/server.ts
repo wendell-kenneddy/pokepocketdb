3daec3c9ef24775ab648b57db13bda7658918b92
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-11-25 08:10:29 -0300

    feat(server): use rate limiter middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import "express-async-errors";
+import rateLimit from "express-rate-limit";
 import helmet from "helmet";
 import { env } from "./lib/env";
 import { errorHandlerMiddleware } from "./middlewares/error-handler-middleware";
@@ -9,10 +10,15 @@ import { expansionsRouter } from "./modules/expansions/expansions-router";
 import { matchResultsRouter } from "./modules/matches/match-results-router";
 
 const app = express();
+const limiter = rateLimit({
+  windowMs: 1000 * 60 * 15,
+  limit: 50
+});
 
 app.use(helmet());
 app.use(cors({ origin: env.ORIGIN }));
 app.use(express.json());
+app.use(limiter);
 
 app.use(expansionsRouter);
 app.use(cardsRouter);
