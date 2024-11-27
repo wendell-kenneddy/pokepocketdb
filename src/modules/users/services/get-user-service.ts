dc5e377c39946442d7c3309a4f6357212e09ea03
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-11-27 15:48:37 -0300

    feat(users/services): add get-user-service

---
+++ src/modules/users/services/get-user-service.ts
@@ -0,0 +1,27 @@
+import { eq } from "drizzle-orm";
+import { db } from "../../../db";
+import { roles, users } from "../../../db/schema";
+import { uuidSchema } from "../../../lib/uuid-schema";
+
+type User = typeof users.$inferSelect;
+export interface UserWithPermissions
+  extends Omit<User, "password" | "createdAt" | "roleId"> {
+  role: string | null;
+}
+
+export class GetUserService {
+  async execute(userId: unknown) {
+    const parsedUserId = uuidSchema.parse(userId);
+    const rows = await db
+      .select({
+        id: users.id,
+        name: users.name,
+        email: users.email,
+        role: roles.name
+      })
+      .from(users)
+      .innerJoin(roles, eq(users.roleId, roles.id))
+      .where(eq(users.id, parsedUserId));
+    return rows[0];
+  }
+}
