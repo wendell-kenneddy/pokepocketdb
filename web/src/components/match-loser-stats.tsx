0b8496dac48a8feb28ff79686dfea5a226f697fd
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-06 12:30:03 -0300

    feat(web/components): add match-loser-stats component

---
+++ web/src/components/match-loser-stats.tsx
@@ -0,0 +1,41 @@
+import { CardData, PokemonType } from "@/data/types";
+import { Card } from "./card";
+
+interface MatchLoserStatsProps {
+  name: string;
+  level: number;
+  energies: PokemonType[];
+  deck: CardData[];
+}
+
+export function MatchLoserStats({ name, energies, deck, level }: MatchLoserStatsProps) {
+  return (
+    <section className="w-full rounded-t-md border border-red-400">
+      <h2 className="sr-only">Match winner{"'"}s stats</h2>
+
+      <header className="bg-gray-900 rounded-t-md h-8 border-b border-b-red-400 px-4 flex items-center justify-between">
+        <span>
+          <span className="sr-only">Name: </span>
+          {name}
+        </span>
+
+        <span>LVL {level}</span>
+
+        <span>Energies: {energies.join(", ")}</span>
+      </header>
+
+      <div className="flex max-w-[720px] overflow-x-auto items-center gap-2">
+        {deck.map((c) => (
+          <Card
+            key={c.id}
+            name={c.name}
+            category={c.category}
+            type={c.type}
+            expansion={c.expansion}
+            variant="loser"
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
