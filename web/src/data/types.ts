e505d843c2a01e0b55974593eeccf4d413e582d5
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-06 12:26:43 -0300

    feat(types): add PokemonType, CardCategory and FullMatchResult types

--- web/src/data/types.ts
+++ web/src/data/types.ts
@@ -1,8 +1,43 @@
+export type CardCategory = "pokemon" | "support" | "item";
+
+export type PokemonType =
+  | "fire"
+  | "grass"
+  | "water"
+  | "lightning"
+  | "fighting"
+  | "metal"
+  | "psychic"
+  | "dark"
+  | "dragon"
+  | "colorless";
+
+export interface CardData {
+  id: string;
+  name: string;
+  category: CardCategory;
+  type: PokemonType | null;
+  expansion: string;
+}
+
 export interface ShortMatchResult {
   id: string;
   winnerName: string;
-  winnerEnergies: string[];
+  winnerEnergies: PokemonType[];
   loserName: string;
-  loserEnergies: string[];
+  loserEnergies: PokemonType[];
   turns: number;
+}
+
+export interface FullMatchResult extends ShortMatchResult {
+  winnerCoinFirst: boolean;
+  winnerTypeAdvantage: boolean;
+  winnerTypeDisadvantage: boolean;
+  winnerPoints: number;
+  winnerLevel: number;
+  loserLevel: number;
+  loserPoints: number;
+  loserConcede: boolean;
+  winnerDeck: CardData[];
+  loserDeck: CardData[];
 }
