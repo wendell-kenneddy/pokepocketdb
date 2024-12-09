2fb69bc9e5be189a91c2c143107b8604c7d22bca
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-09 14:27:10 -0300

    feat(web/pages/match): export metadata

--- web/src/app/matches/[id]/page.tsx
+++ web/src/app/matches/[id]/page.tsx
@@ -2,10 +2,23 @@ import { MatchGeneralStats } from "@/components/match-general-stats";
 import { MatchLoserStats } from "@/components/match-loser-stats";
 import { MatchWinnerStats } from "@/components/match-winner-stats";
 import { mockFullMatchResult } from "@/data/mock";
+import { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}): Promise<Metadata> {
+  const { id } = await params;
+  return {
+    title: `pokepocketDB | Match ${id}`,
+    description: "View full match details.",
+  };
+}
 
 export default async function Page({}: { params: Promise<{ id: string }> }) {
   return (
-    <main className="w-[90%] max-w-[720px] grid place-items-center py-4 h-full mx-auto">
+    <main className="w-[90%] max-w-[720px] grid place-items-center justify-center py-4 h-full mx-auto">
       <div className="w-full flex flex-col items-start gap-4 text-sm">
         <MatchGeneralStats
           winnerCoinFirst={mockFullMatchResult.winnerCoinFirst}
