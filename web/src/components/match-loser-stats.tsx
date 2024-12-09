786f0e28cdfac5991d10a02df710cb78556d8508
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-09 14:26:48 -0300

    feat(web): add responsivity to match-loser-stats component

--- web/src/components/match-loser-stats.tsx
+++ web/src/components/match-loser-stats.tsx
@@ -10,7 +10,7 @@ interface MatchLoserStatsProps {
 
 export function MatchLoserStats({ name, energies, deck, level }: MatchLoserStatsProps) {
   return (
-    <section className="w-full rounded-t-md border border-red-400">
+    <section className="w-full max-w-[336px] xs:max-w-[394px] sm:max-w-[500px] md:max-w-[720px] rounded-t-md border border-red-400">
       <h2 className="sr-only">Match winner{"'"}s stats</h2>
 
       <header className="bg-gray-900 rounded-t-md h-8 border-b border-b-red-400 px-4 flex items-center justify-between">
@@ -24,7 +24,7 @@ export function MatchLoserStats({ name, energies, deck, level }: MatchLoserStats
         <span>Energies: {energies.join(", ")}</span>
       </header>
 
-      <div className="flex max-w-[720px] overflow-x-auto items-center gap-2">
+      <div className="flex max-w-[336px] xs:max-w-[394px] sm:max-w-[500px] md:max-w-[720px] overflow-x-auto items-center gap-2">
         {deck.map((c) => (
           <Card
             key={c.id}
