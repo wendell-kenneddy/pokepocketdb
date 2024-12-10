5265e089d82510382aef2c36516008af71335266
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-10 17:21:06 -0300

    feat(admin): add mock data file

---
+++ admin/src/data/mock.ts
@@ -0,0 +1,54 @@
+import { Expansion } from "./types";
+
+export const mockExpansions: Expansion[] = [
+  {
+    id: "1",
+    name: "Promo-A",
+    createdAt: "10 dez 2024",
+  },
+  {
+    id: "2",
+    name: "Genetic Apex",
+    createdAt: "10 dez 2024",
+  },
+  {
+    id: "3",
+    name: "Genetic Apex",
+    createdAt: "10 dez 2024",
+  },
+  {
+    id: "4",
+    name: "Genetic Apex",
+    createdAt: "10 dez 2024",
+  },
+  {
+    id: "5",
+    name: "Genetic Apex",
+    createdAt: "10 dez 2024",
+  },
+  {
+    id: "6",
+    name: "Genetic Apex",
+    createdAt: "10 dez 2024",
+  },
+  {
+    id: "7",
+    name: "Genetic Apex",
+    createdAt: "10 dez 2024",
+  },
+  {
+    id: "8",
+    name: "Genetic Apex",
+    createdAt: "10 dez 2024",
+  },
+  {
+    id: "9",
+    name: "Genetic Apex",
+    createdAt: "10 dez 2024",
+  },
+  {
+    id: "10",
+    name: "Genetic Apex",
+    createdAt: "10 dez 2024",
+  },
+];
