046b43b2ce856af199a41cb77da9ff32b4058aa3
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-10 17:21:22 -0300

    feat(admin): add sidebar-link component

---
+++ admin/src/components/sidebar-link.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
+
+interface SidebarLinkProps {
+  children: ReactNode;
+  href: string;
+}
+
+export function SidebarLink({ children, href }: SidebarLinkProps) {
+  const pathname = usePathname();
+  const styles =
+    pathname == href ? "text-teal-400 font-medium" : "text-gray-100/50 hover:text-gray-100";
+
+  return (
+    <Link href={href} className={`${styles} text-sm`}>
+      {children}
+    </Link>
+  );
+}
