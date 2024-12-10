2366d4b7f3dfba7621427f2ae0102ec995674da3
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-10 17:19:59 -0300

    feat(admin): add form component

---
+++ admin/src/components/form.tsx
@@ -0,0 +1,14 @@
+import { ComponentProps, FormHTMLAttributes } from "react";
+
+interface FormProps extends ComponentProps<"form"> {}
+
+export function Form({ children, ...props }: FormProps) {
+  return (
+    <form
+      className="w-full max-w-[500px] p-4 rounded-md bg-gray-900 flex flex-col items-center gap-4"
+      {...props}
+    >
+      {children}
+    </form>
+  );
+}
