9a18bfbc4f5d048555284663f6b64ace5c1d3291
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-11 17:14:16 -0300

    feat(admin): increase form component max size to 600px

--- admin/src/components/form.tsx
+++ admin/src/components/form.tsx
@@ -5,7 +5,7 @@ interface FormProps extends ComponentProps<"form"> {}
 export function Form({ children, ...props }: FormProps) {
   return (
     <form
-      className="w-full max-w-[500px] p-4 rounded-md bg-gray-900 flex flex-col items-center gap-4"
+      className="w-full max-w-[600px] p-4 rounded-md bg-gray-900 flex flex-col items-center gap-4"
       {...props}
     >
       {children}
