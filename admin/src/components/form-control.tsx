056134cfb5a5db3aa3b02cbbb26437e04a86340b
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-11 17:21:18 -0300

    refactor(admin): remove label prop from FormControlProps

--- admin/src/components/form-control.tsx
+++ admin/src/components/form-control.tsx
@@ -1,19 +1,18 @@
 import { ComponentProps, InputHTMLAttributes } from "react";
 
 interface FormControlProps extends ComponentProps<"input"> {
-  label: string;
   labelFor: string;
 }
 
-export function FormControl({ label, labelFor, ...props }: FormControlProps) {
+export function FormControl({ children, labelFor, ...props }: FormControlProps) {
   return (
     <div className="w-full">
       <label htmlFor={labelFor} className="sr-only">
-        {label}
+        {children}
       </label>
 
       <input
-        className="w-full px-4 py-2 text-gray-100 bg-gray-900 outline-none ring-1 ring-gray-950 focus:bg-gray-950 focus:ring-teal-400 invalid:ring-red-400"
+        className="w-full px-4 h-10 text-gray-100 bg-gray-900 outline-none ring-1 ring-gray-950 focus:bg-gray-950 focus:ring-teal-400 invalid:ring-red-400"
         {...props}
       />
     </div>
