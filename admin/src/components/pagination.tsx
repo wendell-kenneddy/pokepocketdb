a5ece741d56ae3bbd8da0b4c17c986984366d2b4
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-13 17:24:03 -0300

    feat(admin): update pagination component to better suit search params usage

--- admin/src/components/pagination.tsx
+++ admin/src/components/pagination.tsx
@@ -1,41 +1,45 @@
+import Link from "next/link";
 import { FiArrowLeft, FiArrowRight } from "react-icons/fi";
 
 interface PaginationProps {
-  getNextPage: () => void;
-  getPrevPage: () => void;
+  basePath: string;
   canGoBack: boolean;
   canGoForward: boolean;
   page: number;
 }
 
-export function Pagination({
-  getPrevPage,
-  getNextPage,
-  canGoBack,
-  canGoForward,
-  page,
-}: PaginationProps) {
+export function Pagination({ basePath, canGoBack, canGoForward, page }: PaginationProps) {
   return (
     <div className="bg-gray-900 border-t border-t-teal-400 py-1 px-4 rounded-b-md w-full flex items-center justify-between">
-      <button
-        className="disabled:cursor-not-allowed disabled:opacity-50"
-        name="View previous 10 match results"
-        onClick={getPrevPage}
-        disabled={!canGoBack}
-      >
-        <FiArrowLeft size={16} className="text-teal-400" />
-      </button>
+      {canGoBack ? (
+        <Link href={`${basePath}?page=${page - 1}`}>
+          <FiArrowLeft size={16} className="text-teal-400" />
+        </Link>
+      ) : (
+        <button
+          className="cursor-not-allowed opacity-50"
+          name="View previous 10 match results"
+          disabled
+        >
+          <FiArrowLeft size={16} className="text-teal-400" />
+        </button>
+      )}
 
       <span about="Current match results page">{page}</span>
 
-      <button
-        className="disabled:cursor-not-allowed disabled:opacity-50"
-        name="View next 10 match results"
-        onClick={getNextPage}
-        disabled={!canGoForward}
-      >
-        <FiArrowRight size={16} className="text-teal-400" />
-      </button>
+      {canGoForward ? (
+        <Link href={`${basePath}?page=${page + 1}`}>
+          <FiArrowRight size={16} className="text-teal-400" />
+        </Link>
+      ) : (
+        <button
+          className="cursor-not-allowed opacity-50"
+          name="View next 10 match results"
+          disabled
+        >
+          <FiArrowRight size={16} className="text-teal-400" />
+        </button>
+      )}
     </div>
   );
 }
