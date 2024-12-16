dc779ca039253099d8488e74a5de2d742016dc1e
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-16 17:59:21 -0300

    feat(backend): add redisClient

---
+++ backend/src/lib/redis.ts
@@ -0,0 +1,9 @@
+import { createClient } from "redis";
+import { env } from "./env";
+
+const redisClient = createClient({
+  url: env.REDIS_URL
+});
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+
+export { redisClient };
