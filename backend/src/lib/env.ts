704120fa8acb4f91aa20eadf2828f3a167dab14e
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-16 18:00:19 -0300

    feat(backend): add REDIS_URL to env util

--- backend/src/lib/env.ts
+++ backend/src/lib/env.ts
@@ -3,6 +3,7 @@ import z from "zod";
 const schema = z.object({
   PORT: z.string(),
   ORIGIN: z.string(),
+  REDIS_URL: z.string().url(),
   ACCESS_TOKEN_SECRET: z.string(),
   REFRESH_TOKEN_SECRET: z.string(),
   COOKIE_SECRET: z.string(),
