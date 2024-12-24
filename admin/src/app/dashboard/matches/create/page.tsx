d2489a169ff0536fb37d9fdf5a1cf9d2c787e436
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-24 17:54:19 -0300

    feat(admin): fetch auxiliary data on match results creation route

--- admin/src/app/dashboard/matches/create/page.tsx
+++ admin/src/app/dashboard/matches/create/page.tsx
@@ -1,17 +1,42 @@
 import { CreateMatchResultForm } from "@/components/create-match-result-form";
-import { mockCards, mockPokemonTypes } from "@/data/mock";
+import { SelectOption } from "@/components/form-select-control";
+import { APIResponse, Card } from "@/data/types";
+import { capitalizeFirstLetter } from "@/lib/capitalize-first-letter";
+import { refetchableRequest } from "@/lib/refetchable-request";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "pokepocketDB Admin | Insert match result",
 };
 
-export default function CreateMatch() {
-  return (
-    <main className="w-[90%] max-w-[720px] mx-auto py-4">
-      <h2 className="text-lg font-medium sr-only">Create expansion</h2>
+export default async function CreateMatch() {
+  const [types, cards] = await Promise.all([
+    refetchableRequest<APIResponse<string[]>>(`${process.env.API_URL}/cards/types`, {
+      cache: "force-cache",
+      next: {
+        tags: ["types:all"],
+      },
+    }),
+    refetchableRequest<APIResponse<Card[]>>(`${process.env.API_URL}/cards?page=1&limit=1000`, {
+      cache: "force-cache",
+      next: {
+        tags: ["cards:all"],
+      },
+    }),
+  ]);
+  const pokemonTypesSelectOptions: SelectOption[] = types.json.data.map((t) => ({
+    value: t,
+    displayValue: capitalizeFirstLetter(t),
+  }));
 
-      <CreateMatchResultForm pokemonTypes={mockPokemonTypes} availableCards={mockCards} />
+  return (
+    <main className="w-[90%] overflow-y-auto max-w-[720px] flex flex-col items-center gap-4 justify-center mx-auto py-4">
+      <h2 className="sr-only">Create expansion</h2>
+
+      <CreateMatchResultForm
+        pokemonTypes={pokemonTypesSelectOptions}
+        availableCards={cards.json.data}
+      />
     </main>
   );
 }
