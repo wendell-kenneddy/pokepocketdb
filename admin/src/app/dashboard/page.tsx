f8d13abe592e00331ec6f2a5a0df3bedf5e051f9
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-24 17:49:50 -0300

    feat(admin): add some content to dashboard homepage

--- admin/src/app/dashboard/page.tsx
+++ admin/src/app/dashboard/page.tsx
@@ -1,3 +1,26 @@
-export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "pokepocketDB Admin | Dashboard",
+  description: "Access and manage all your data.",
+};
+
+export default async function Dashboard() {
+  return (
+    <main className="w-[90%] max-w-[500px] mx-auto flex flex-col items-start py-4 gap-4">
+      <h1 className="sr-only">Dashboard page</h1>
+
+      <p className="text-justify">
+        Here you can access and manage all data from pokepocketDB. Not sure where to start? Maybe{" "}
+        <Link
+          className="text-teal-400 underline hover:no-underline"
+          href="/dashboard/expansions/create"
+        >
+          try creating an Expansion first
+        </Link>
+        .
+      </p>
+    </main>
+  );
 }
