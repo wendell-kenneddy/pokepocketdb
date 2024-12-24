b88187ff88b567fe3318f6165b5d3248a57aa7bc
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-24 17:52:33 -0300

    feat(admin): fetch real data to display on expansions homepage

--- admin/src/app/dashboard/expansions/page.tsx
+++ admin/src/app/dashboard/expansions/page.tsx
@@ -1,15 +1,27 @@
 import { ExpansionsContent } from "@/components/expansions-content";
-import { mockExpansions } from "@/data/mock";
+import { APIResponse, Expansion, SearchParams } from "@/data/types";
+import { getPageFromSearchParams } from "@/lib/get-page-from-search-params";
+import { refetchableRequest } from "@/lib/refetchable-request";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "pokepocketDB Admin | Expansions",
 };
 
-export default function Expansions() {
+export default async function Expansions({
+  searchParams,
+}: {
+  searchParams: Promise<SearchParams>;
+}) {
+  const params = await searchParams;
+  const page = getPageFromSearchParams(params, 1);
+  const { json } = await refetchableRequest<APIResponse<Expansion[]>>(
+    `${process.env.API_URL}/expansions?page=${page}&limit=10`
+  );
+
   return (
     <main className="w-[90%] max-w-[720px] mx-auto py-4">
-      <ExpansionsContent initialExpansions={mockExpansions} />
+      <ExpansionsContent page={page} expansions={json.data} />
     </main>
   );
 }
