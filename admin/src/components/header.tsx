a15576f747ca4dcafdfc7f8843eb56214bb9b0c1
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-24 17:26:25 -0300

    feat(admin): update header content based on user session

--- admin/src/components/header.tsx
+++ admin/src/components/header.tsx
@@ -1,13 +1,29 @@
+import { UserSession } from "@/data/types";
 import Image from "next/image";
 import Link from "next/link";
+import { LogoutButton } from "./logout-button";
 
-export function Header() {
+interface HeaderProps {
+  session?: UserSession;
+}
+
+export function Header({ session }: HeaderProps) {
+  const justify = session ? "justify-between" : "justify-center";
+
   return (
     <header className="bg-gray-900 h-full w-full border-b border-b-gray-800">
-      <div className="w-[90%] h-full mx-auto max-w-[720px] flex items-center justify-center">
+      <div className={`w-[90%] h-full mx-auto max-w-[720px] flex items-center ${justify}`}>
         <Link href="/dashboard">
           <Image src="/logo-admin.svg" alt="Logo" priority width={170} height={24} />
         </Link>
+
+        {session && (
+          <div className="flex items-center justify-start gap-2">
+            <span className="text-sm">{session.email}</span>
+
+            <LogoutButton />
+          </div>
+        )}
       </div>
     </header>
   );
