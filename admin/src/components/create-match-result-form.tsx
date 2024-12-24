8fb1c0b896e639c5feff8bfbfce72084784288e2
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-24 17:48:15 -0300

    feat(admin): add proper logic to create-match-result-form

--- admin/src/components/create-match-result-form.tsx
+++ admin/src/components/create-match-result-form.tsx
@@ -4,13 +4,14 @@ import { Card } from "@/data/types";
 import { Form } from "./form";
 import { FormControl } from "./form-control";
 import { SelectOption } from "./form-select-control";
-import { RadioGroup } from "./radio-group";
 import { Checkbox } from "./checkbox";
-import { useState } from "react";
+import { useActionState, useState } from "react";
 import { CheckedState } from "@radix-ui/react-checkbox";
 import { DeckBuilder } from "./deck-builder";
 import { CardWithCount } from "./player-deck-card";
 import { Button } from "./button";
+import { createMatchResultAction } from "@/actions/create-match-result-action";
+import { RadioGroup } from "./radio-group";
 
 interface CreateMatchResultFormProps {
   pokemonTypes: SelectOption[];
@@ -25,6 +26,7 @@ export function CreateMatchResultForm({
   const [checkedLoserEnergies, setCheckedLoserEnergies] = useState<string[]>([]);
   const [winnerDeck, setWinnerDeck] = useState<CardWithCount[]>([]);
   const [loserDeck, setLoserDeck] = useState<CardWithCount[]>([]);
+  const [state, formAction, pending] = useActionState(createMatchResultAction, null);
 
   function handleWinnerEnergyCheckboxChange(checked: CheckedState, id: string) {
     if (checked === true) {
@@ -56,156 +58,222 @@ export function CreateMatchResultForm({
   }
 
   return (
-    <Form>
-      <FormControl
-        labelFor="turns"
-        labelVisible={true}
-        name="turns"
-        id="turns"
-        type="number"
-        min={1}
-        max={30}
-        step={1}
-      >
-        Turns
-      </FormControl>
+    <>
+      {state?.message.length && !pending ? (
+        <div className="w-full max-w-[600px] bg-teal-600 border border-teal-400 rounded-md py-2 px-4 flex flex-col items-start gap-1">
+          <span className="text-teal-200 font-medium">{state.message}</span>
+        </div>
+      ) : (
+        ""
+      )}
 
-      <div className="grid grid-cols-[70%_30%] items-center gap-4">
+      {state?.errors?.length && !pending ? (
+        <ul className="w-full max-w-[600px] bg-red-600 border border-red-400 rounded-md py-2 px-4 flex flex-col items-start gap-1">
+          {state.errors.map((err, idx) => (
+            <li key={idx} className="text-red-200 font-medium">
+              {err}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        ""
+      )}
+
+      <Form action={formAction}>
+        <input type="hidden" name="winner-energies" value={JSON.stringify(checkedWinnerEnergies)} />
+        <input type="hidden" name="loser-energies" value={JSON.stringify(checkedLoserEnergies)} />
+        <input
+          type="hidden"
+          name="winner-deck"
+          value={JSON.stringify(winnerDeck.map(({ id, count }) => ({ cardId: id, count })))}
+        />
+        <input
+          type="hidden"
+          name="loser-deck"
+          value={JSON.stringify(loserDeck.map(({ id, count }) => ({ cardId: id, count })))}
+        />
+
+        <FormControl
+          labelFor="turns"
+          labelVisible={true}
+          name="turns"
+          id="turns"
+          type="number"
+          defaultValue={1}
+          min={1}
+          max={30}
+          step={1}
+        >
+          Turns
+        </FormControl>
+
+        <div className="grid grid-cols-3 items-center gap-4">
+          <Checkbox id="winner-coin-first" name="winner-coin-first">
+            Winner went first?
+          </Checkbox>
+
+          <Checkbox id="loser-concede" name="loser-concede">
+            Loser conceded?
+          </Checkbox>
+
+          <Checkbox id="winner-comeback" name="winner-comeback">
+            Winner comeback?
+          </Checkbox>
+        </div>
+
         <RadioGroup
-          ariaLabel="First player of the match"
-          defaultValue="winner-first"
+          name="advantages"
+          ariaLabel="Type relations"
+          defaultValue="neutral"
           items={[
-            { id: "winner-first", displayValue: "Winner went first", value: "winner-first" },
-            { id: "loser-first", displayValue: "Loser went first", value: "loser-first" },
+            {
+              value: "winner_advantage",
+              id: "winner_advantage",
+              displayValue: "Winner had type advantage",
+            },
+            {
+              value: "neutral",
+              id: "neutral",
+              displayValue: "Both were type neutral",
+            },
+            {
+              value: "loser_advantage",
+              id: "loser_advantage",
+              displayValue: "Loser had type advantage",
+            },
           ]}
         />
 
-        <Checkbox id="loser-concede">Loser conceded?</Checkbox>
-      </div>
+        <FormControl
+          labelFor="winner-name"
+          labelVisible
+          name="winner-name"
+          id="winner-name"
+          type="text"
+          minLength={1}
+        >
+          Winner's name
+        </FormControl>
 
-      <FormControl
-        labelFor="winner-name"
-        labelVisible
-        name="winner-name"
-        id="winner-name"
-        type="text"
-        minLength={1}
-      >
-        Winner's name
-      </FormControl>
+        <FormControl
+          labelFor="winner-level"
+          labelVisible
+          name="winner-level"
+          id="winner-level"
+          type="number"
+          defaultValue={1}
+          min={1}
+          max={50}
+          step={1}
+        >
+          Winner's level
+        </FormControl>
 
-      <FormControl
-        labelFor="winner-level"
-        labelVisible
-        name="winner-level"
-        id="winner-level"
-        type="number"
-        min={1}
-        max={50}
-        step={1}
-      >
-        Winner's level
-      </FormControl>
+        <FormControl
+          labelFor="winner-points"
+          labelVisible
+          name="winner-points"
+          id="winner-points"
+          type="number"
+          defaultValue={0}
+          min={0}
+          max={3}
+          step={1}
+        >
+          Winner's points
+        </FormControl>
 
-      <FormControl
-        labelFor="winner-points"
-        labelVisible
-        name="winner-points"
-        id="winner-points"
-        type="number"
-        min={0}
-        max={3}
-        step={1}
-      >
-        Winner's points
-      </FormControl>
+        <DeckBuilder
+          availableCards={availableCards}
+          deck={winnerDeck}
+          updateDeckFn={setWinnerDeck}
+          player="winner"
+        />
 
-      <DeckBuilder
-        availableCards={availableCards}
-        deck={winnerDeck}
-        updateDeckFn={setWinnerDeck}
-        player="winner"
-      />
+        <div className="flex items-center justify-around w-full">
+          <div className="flex flex-col items-start gap-1">
+            <h3 className="text-sm">Winner energies</h3>
+            {pokemonTypes.map((t) => (
+              <Checkbox
+                onCheckedChange={(c) =>
+                  handleWinnerEnergyCheckboxChange(c, `winner-energy-${t.value}`)
+                }
+                disabled={isEnergyCheckboxDisabled("winner", `winner-energy-${t.value}`)}
+                key={`winner-${t.value}`}
+                id={`winner-energy-${t.value}`}
+              >
+                {t.displayValue}
+              </Checkbox>
+            ))}
+          </div>
 
-      <div className="flex items-center justify-around w-full">
-        <div className="flex flex-col items-start gap-1">
-          <h3 className="text-sm">Winner energies</h3>
-          {pokemonTypes.map((t) => (
-            <Checkbox
-              onCheckedChange={(c) =>
-                handleWinnerEnergyCheckboxChange(c, `winner-energy-${t.value}`)
-              }
-              disabled={isEnergyCheckboxDisabled("winner", `winner-energy-${t.value}`)}
-              key={`winner-${t.value}`}
-              id={`winner-energy-${t.value}`}
-            >
-              {t.displayValue}
-            </Checkbox>
-          ))}
+          <div className="flex flex-col items-start gap-1">
+            <h3 className="text-sm">Loser energies</h3>
+            {pokemonTypes.map((t) => (
+              <Checkbox
+                onCheckedChange={(c) =>
+                  handleLoserEnergyCheckboxChange(c, `loser-energy-${t.value}`)
+                }
+                disabled={isEnergyCheckboxDisabled("loser", `loser-energy-${t.value}`)}
+                key={`loser-${t.value}`}
+                id={`loser-energy-${t.value}`}
+              >
+                {t.displayValue}
+              </Checkbox>
+            ))}
+          </div>
         </div>
 
-        <div className="flex flex-col items-start gap-1">
-          <h3 className="text-sm">Loser energies</h3>
-          {pokemonTypes.map((t) => (
-            <Checkbox
-              onCheckedChange={(c) => handleLoserEnergyCheckboxChange(c, `loser-energy-${t.value}`)}
-              disabled={isEnergyCheckboxDisabled("loser", `loser-energy-${t.value}`)}
-              key={`loser-${t.value}`}
-              id={`loser-energy-${t.value}`}
-            >
-              {t.displayValue}
-            </Checkbox>
-          ))}
-        </div>
-      </div>
+        <DeckBuilder
+          availableCards={availableCards}
+          deck={loserDeck}
+          updateDeckFn={setLoserDeck}
+          player="loser"
+        />
 
-      <DeckBuilder
-        availableCards={availableCards}
-        deck={loserDeck}
-        updateDeckFn={setLoserDeck}
-        player="loser"
-      />
+        <FormControl
+          labelFor="loser-name"
+          labelVisible
+          name="loser-name"
+          id="loser-name"
+          type="text"
+          minLength={1}
+        >
+          Loser's name
+        </FormControl>
 
-      <FormControl
-        labelFor="loser-name"
-        labelVisible
-        name="loser-name"
-        id="loser-name"
-        type="text"
-        minLength={1}
-      >
-        Loser's name
-      </FormControl>
+        <FormControl
+          labelFor="loser-level"
+          labelVisible
+          name="loser-level"
+          id="loser-level"
+          type="number"
+          defaultValue={1}
+          min={1}
+          max={50}
+          step={1}
+        >
+          Loser's level
+        </FormControl>
 
-      <FormControl
-        labelFor="loser-level"
-        labelVisible
-        name="loser-level"
-        id="loser-level"
-        type="number"
-        min={1}
-        max={50}
-        step={1}
-      >
-        Loser's level
-      </FormControl>
+        <FormControl
+          labelFor="loser-points"
+          labelVisible
+          name="loser-points"
+          id="loser-points"
+          type="number"
+          defaultValue={0}
+          min={0}
+          max={2}
+          step={1}
+        >
+          Loser's points
+        </FormControl>
 
-      <FormControl
-        labelFor="loser-points"
-        labelVisible
-        name="loser-points"
-        id="loser-points"
-        type="number"
-        min={0}
-        max={2}
-        step={1}
-      >
-        Loser's points
-      </FormControl>
-
-      <Button w="max" type="submit">
-        Create
-      </Button>
-    </Form>
+        <Button disabled={pending} w="max" type="submit">
+          {pending ? "Creating..." : "Create"}
+        </Button>
+      </Form>
+    </>
   );
 }
