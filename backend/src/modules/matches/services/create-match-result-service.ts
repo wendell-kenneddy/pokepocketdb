eb82486500a303fa98fb30a6e5aaf9bea802ec6a
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-24 15:29:15 -0300

    refactor(backend): update create-match-result-service according to schema changes

--- backend/src/modules/matches/services/create-match-result-service.ts
+++ backend/src/modules/matches/services/create-match-result-service.ts
@@ -2,10 +2,17 @@ import { createInsertSchema } from "drizzle-zod";
 import z from "zod";
 import { db } from "../../../db";
 import { matchCards, matchResults, pokemonTypesEnum } from "../../../db/schema";
-import { ValidationError } from "../../../errors/validation-error";
 import { uuidSchema } from "../../../lib/uuid-schema";
 
 const zEnergyEnum = z.enum(pokemonTypesEnum.enumValues);
+
+const matchCardSchema = z.object({
+  cardId: uuidSchema,
+  count: z
+    .number()
+    .min(1, "A single card count must be at least 1.")
+    .max(2, "A single card can't be used more than twice.")
+});
 
 const matchResultSchema = createInsertSchema(matchResults, {
   winnerEnergies: zEnergyEnum
@@ -18,21 +25,20 @@ const matchResultSchema = createInsertSchema(matchResults, {
 
 const createMatchResultSchema = z.object({
   matchResult: matchResultSchema,
-  winnerDeck: uuidSchema
+  winnerDeck: matchCardSchema
     .array()
     .min(1, "Winner's deck must contain at least 1 card.")
-    .max(20, "A deck can't contain more than 20 cards."),
-  loserDeck: uuidSchema
+    .max(10, "A deck can't contain more than 20 cards."),
+  loserDeck: matchCardSchema
     .array()
     .min(1, "Loser's deck must contain at least 1 card.")
-    .max(20, "A deck can't contain more than 20 cards.")
+    .max(10, "A deck can't contain more than 20 cards.")
 });
 
 export class CreateMatchResultService {
   async execute(data: unknown) {
     const { matchResult, winnerDeck, loserDeck } =
       createMatchResultSchema.parse(data);
-    this.validateDecks(winnerDeck, loserDeck);
 
     const matchId = await db.transaction(async (tx) => {
       const [{ id: matchId }] = await tx
@@ -40,8 +46,18 @@ export class CreateMatchResultService {
         .values(matchResult)
         .returning({ id: matchResults.id });
       const cards = [
-        ...winnerDeck.map((cardId) => ({ cardId, matchId, winnerCard: true })),
-        ...loserDeck.map((cardId) => ({ cardId, matchId, winnerCard: false }))
+        ...winnerDeck.map(({ cardId, count }) => ({
+          matchId,
+          cardId,
+          count,
+          winnerCard: true
+        })),
+        ...loserDeck.map(({ cardId, count }) => ({
+          matchId,
+          cardId,
+          count,
+          winnerCard: false
+        }))
       ];
 
       await tx.insert(matchCards).values(cards);
@@ -50,24 +66,5 @@ export class CreateMatchResultService {
     });
 
     return matchId;
-  }
-
-  private validateDecks(winnerDeck: string[], loserDeck: string[]) {
-    winnerDeck.reduce<Record<string, number>>((acc, val) => {
-      if (acc[val] == 2)
-        throw new ValidationError(
-          "Winner's deck contain more than two of the same card(s)."
-        );
-      acc[val] = acc[val] ? acc[val] + 1 : 1;
-      return acc;
-    }, {});
-    loserDeck.reduce<Record<string, number>>((acc, val) => {
-      if (acc[val] == 2)
-        throw new Error(
-          "Winner's deck contain more than two of the same card(s)."
-        );
-      acc[val] = acc[val] ? acc[val] + 1 : 1;
-      return acc;
-    }, {});
   }
 }
