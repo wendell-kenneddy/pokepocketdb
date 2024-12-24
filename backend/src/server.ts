bd588bb9268017b84ba107980c068138ceb42731
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-24 15:19:08 -0300

    refactor(backend): use rate limiter on prod only

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,7 @@ import "express-async-errors";
 import rateLimit from "express-rate-limit";
 import helmet from "helmet";
 import { env } from "./lib/env";
+import { redisClient } from "./lib/redis";
 import { errorHandlerMiddleware } from "./middlewares/error-handler-middleware";
 import { authRouter } from "./modules/auth/auth-router";
 import { cardsRouter } from "./modules/cards/cards-router";
@@ -12,19 +13,21 @@ import { expansionsRouter } from "./modules/expansions/expansions-router";
 import { matchResultsRouter } from "./modules/matches/match-results-router";
 import { rolesRouter } from "./modules/roles/roles-router";
 import { usersRouter } from "./modules/users/users-router";
-import { redisClient } from "./lib/redis";
 
 const app = express();
-const limiter = rateLimit({
-  windowMs: 1000 * 60 * 15,
-  limit: 50
-});
 
 app.use(helmet());
 app.use(cors({ origin: env.ORIGIN }));
 app.use(cookieParser(env.COOKIE_SECRET));
 app.use(express.json());
-app.use(limiter);
+
+if (process.env.NODE_ENV == "production") {
+  const limiter = rateLimit({
+    windowMs: 1000 * 60 * 15,
+    limit: 50
+  });
+  app.use(limiter);
+}
 
 app.use(expansionsRouter);
 app.use(cardsRouter);
@@ -37,8 +40,8 @@ app.use(errorHandlerMiddleware);
 redisClient
   .connect()
   .then(() => {
-    app.listen(env.PORT, () => {
-      console.log(`Server running on ${env.API_BASE_URL}:${env.PORT}`);
+    app.listen(env.API_PORT, () => {
+      console.log(`Server running on ${env.API_BASE_URL}:${env.API_PORT}`);
     });
   })
   .catch((e) => console.log(e));
