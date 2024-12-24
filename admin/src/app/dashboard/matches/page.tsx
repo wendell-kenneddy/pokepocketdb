243ec9c3c606d618789a63af99025b32058dfc7a
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-12-24 17:53:38 -0300

    feat(admin): fetch real data to display on matches route homepage

--- admin/src/app/dashboard/matches/page.tsx
+++ admin/src/app/dashboard/matches/page.tsx
@@ -1,15 +1,23 @@
 import { MatchesContent } from "@/components/matches-content";
-import { mockMatchResults } from "@/data/types";
+import { APIResponse, SearchParams, ShortMatchResult } from "@/data/types";
+import { getPageFromSearchParams } from "@/lib/get-page-from-search-params";
+import { refetchableRequest } from "@/lib/refetchable-request";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "pokepocketDB Admin | Matches",
 };
 
-export default function Matches() {
+export default async function Matches({ searchParams }: { searchParams: Promise<SearchParams> }) {
+  const params = await searchParams;
+  const page = getPageFromSearchParams(params, 1);
+  const { json } = await refetchableRequest<APIResponse<ShortMatchResult[]>>(
+    `${process.env.API_URL}/matches?page=${page}&limit=10`
+  );
+
   return (
     <main className="w-[90%] max-w-[720px] mx-auto py-4">
-      <MatchesContent initialMatchResults={mockMatchResults} />
+      <MatchesContent matchResults={json.data} page={page} />
     </main>
   );
 }
